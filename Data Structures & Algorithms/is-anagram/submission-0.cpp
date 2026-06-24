07f801d209656cc799126070ee43ec95e88218f0
Jairdan Chopra <jairdanchopra@gmail.com>
2026-06-24 16:48:48 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //Populate a hashmap with <key (char), occurances <int>
+        std::unordered_map<char, int> stored;
+        for (const char& c : s) {
+            if (stored.find(c) == stored.end()) {
+                stored[c] = 1;
+            } else {
+                stored[c] += 1;
+            }
+        }
+
+        //Now clear the hashmap
+        for (const char& c : t) {
+            if (stored.find(c) == stored.end()) {
+                return false;
+            } else if (stored[c] <= 0 ) {
+                return false;
+            }
+            else {
+                stored[c] -= 1;
+            }
+        }
+
+        for (const auto& [character, occurs] : stored) {
+            if (occurs >= 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
